d3c1db0a741fdd18eee3dc9fdbade9dc3a821a34
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-10 19:31:51 +0300

    add games controller class
    - GetGames: Gets a list of all active games
    - CreateGame: Creates a new game
    - GetGame: Gets the details of a game by id
    - MakeMove: Makes a move in the game

---
+++ SenseCapitalTestAssignment/Controllers/GamesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SenseCapitalTestAssignment.Services;
+
+namespace SenseCapitalTestAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGamesAsync()
+        {
+            var games = await _gameService.GetGamesAsync();
+            return Ok(games);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Game>> CreateGameAsync()
+        {
+            var game = await _gameService.CreateGameAsync();
+            return Ok(game);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGameAsync(string id)
+        {
+            var game = await _gameService.GetGameAsync(id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult<Game>> MakeMoveAsync(string id, [FromBody] MoveRequest moveRequest)
+        {
+            var game = await _gameService.GetGameAsync(id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            var updatedGame = await _gameService.MakeMoveAsync(game, moveRequest);
+
+            if (updatedGame == null)
+            {
+                return BadRequest("Invalid move.");
+            }
+
+            return Ok(updatedGame);
+        }
+    }
+}
