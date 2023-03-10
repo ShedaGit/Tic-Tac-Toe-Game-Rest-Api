d1201f8682f6e2f0f4d987ab3bb2f754f231a15d
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-10 21:38:02 +0300

    fix type of the game's prop

--- SenseCapitalTestAssignment/Models/Game.cs
+++ SenseCapitalTestAssignment/Models/Game.cs
@@ -5,7 +5,7 @@
         public string Id { get; set; }
         public string Board { get; set; }
         public string NextPlayer { get; set; }
-        public string Winner { get; set; }
+        public string? Winner { get; set; }
         public bool IsDraw { get; set; }
         public bool IsGameOver { get; set; }
     }
