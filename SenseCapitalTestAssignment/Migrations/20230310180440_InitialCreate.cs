3cff4bf4f99e628c3213ce149fac28af8c44197b
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-10 21:36:56 +0300

    add initial migrations

---
+++ SenseCapitalTestAssignment/Migrations/20230310180440_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SenseCapitalTestAssignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Board = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NextPlayer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Winner = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDraw = table.Column<bool>(type: "bit", nullable: false),
+                    IsGameOver = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
