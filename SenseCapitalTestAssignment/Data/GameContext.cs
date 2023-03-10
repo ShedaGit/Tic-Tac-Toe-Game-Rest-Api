025082d33d560f06c230f7275f2d1ad0014979dc
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-10 19:26:16 +0300

    add database connection

---
+++ SenseCapitalTestAssignment/Data/GameContext.cs
@@ -0,0 +1,11 @@
+﻿namespace SenseCapitalTestAssignment.Data
+{
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
