2a83d90269579a4592b503a30a816635ebe706a3
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-03-10 19:07:16 +0300

    add game model class for the tictactoe game

---
+++ SenseCapitalTestAssignment/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace SenseCapitalTestAssignment.Models
+{
+    public class Game
+    {
+        public string Id { get; set; }
+        public string Board { get; set; }
+        public string NextPlayer { get; set; }
+        public string Winner { get; set; }
+        public bool IsDraw { get; set; }
+        public bool IsGameOver { get; set; }
+    }
+}
